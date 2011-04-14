eaadf46a0d069585c13a007a04ecd7dbffb92c5e
bill.lv89 <bill.lv89@gmail.com>
2011-04-14 16:14:43 +0000

    完成了Sohu API的初步开发，包括调整后的接口。

--- src/com/ade/restapi/SohuUpdate.java
+++ src/com/ade/restapi/SohuUpdate.java
@@ -1,9 +1,11 @@
 package com.ade.restapi;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.Header;
 import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 
 import com.ade.site.Site;
 
@@ -13,15 +15,18 @@ import com.ade.site.Site;
  * @created 10-����-2011 ���� 08:29:16
  */
 public class SohuUpdate extends UpdateInterface {
-
-
+	//The Common string of the multipart/form-data encoding format
+	private static final String mf1="Content-Type: multipart/form-data; boundary=---------------------------";
+	private static final String mf2="\r\nContent-Disposition: form-data; name=\"status\"";	
+	protected String rn="\r\n";
+	protected String L="---------------------------";
 	/**
 	 * 
 	 * @param text
 	 * @param site
 	 */
 	protected String getUrl(String text, Site site){
-		return "";
+		return "http://api.t.sohu.com/statuses/upload.json";
 	}
 
 	/**
@@ -32,11 +37,27 @@ public class SohuUpdate extends UpdateInterface {
 	protected Header[] getHeader(String text, Site site){
 		return null;
 	}
-
+	protected byte[] getPostData(String text, Site site){
+		//According to multipart / form-data encoding data structure organized into upstream
+		String Boundary=site.getBoundary();
+		StringBuilder dat=new StringBuilder(mf1);
+		dat.append(Boundary);
+		dat.append(rn);
+		dat.append(L);
+		dat.append(Boundary);
+		dat.append(mf2);
+		dat.append(rn);
+		dat.append(text);
+		dat.append(rn);
+		dat.append(L);
+		dat.append(Boundary);
+		return dat.toString().getBytes();
+	}
 	@Override
 	protected List<NameValuePair> getParams(String text, Site site) {
-		return null;
+		List<NameValuePair> params=new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("text",text));
+		return params;
 	}
-
 
 }
--- src/com/ade/restapi/SohuUpload.java
+++ src/com/ade/restapi/SohuUpload.java
@@ -1,9 +1,17 @@
 package com.ade.restapi;
 
+import java.io.*;
+
+import java.util.logging.Filter;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.Header;
 import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.graphics.Path.FillType;
 
 import com.ade.site.Site;
 
@@ -13,7 +21,12 @@ import com.ade.site.Site;
  * @created 10-����-2011 ���� 08:29:18
  */
 public class SohuUpload extends UploadInterface {
-
+	//The Common string of the multipart/form-data encoding format
+	private static final String mf1="Content-Type: multipart/form-data; boundary=---------------------------";
+	private static final String mf2="\r\nContent-Disposition: form-data; name=\"pic\"; filename=";	
+	private static final String mf3="\r\nContent-Type: image/jpeg\r\n\r\n";
+	protected String rn="\r\n";
+	protected String L="---------------------------";
 	/**
 	 * 
 	 * @param fileName
@@ -21,7 +34,7 @@ public class SohuUpload extends UploadInterface {
 	 * @param site
 	 */
 	protected String getUrl(String fileName, String text, Site site){
-		return "";
+		return "http://api.t.sohu.com/statuses/upload.json";
 	}
 
 	/**
@@ -41,13 +54,45 @@ public class SohuUpload extends UploadInterface {
 	 * @param site
 	 */
 	protected byte[] getPostData(String fileName, String text, Site site){
-		return null;
+		//According to the data file names read in, then follow the multipart / form-data encoding data structure organized into upstream
+		String Q="\"";
+		String Boundary=site.getBoundary();
+		StringBuilder dat=new StringBuilder(mf1);
+		dat.append(Boundary);
+		dat.append(rn);
+		dat.append(L);
+		dat.append(Boundary);
+		dat.append(mf2);
+		dat.append(Q);
+		dat.append(fileName);
+		dat.append(Q);
+		dat.append(mf3);
+		File file= new File(fileName);
+		InputStream in = null;
+		//To read the contents of the file in bytes
+		try {
+			in = new FileInputStream(file);
+			int tempbyte;
+			while((tempbyte=in.read()) != -1){
+				dat.append(tempbyte);
+				}
+			in.close();
+			} catch (IOException e) {
+		      e.printStackTrace();
+		      }
+		dat.append(rn);
+		dat.append(L);
+		dat.append(Boundary);
+		return dat.toString().getBytes();
 	}
 
 	@Override
 	protected List<NameValuePair> getParams(String fileName, String text,
 			Site site) {
-		return null;
+		List<NameValuePair> params=new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("fileName",fileName));
+		params.add(new BasicNameValuePair("text",text));
+		return params;
 	}
 
 }
