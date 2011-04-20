6b93725b809306a49d4aef7c23897fdbb4a0e4da
JordanXu5213@gmail.com <JordanXu5213@gmail.com@1669ee13-3d97-0e45-1c2c-9cf43825c4fe>
2011-04-20 12:31:42 +0000

    重新做了MainActivity.java。现在可以上传了，这里面你们写的内容没动。请放心，这个和刚才上传的部分，做了Main，Write,ReandFW，3个界面。

--- trunk/src/com/ade/soda/MainActivity.java
+++ trunk/src/com/ade/soda/MainActivity.java
@@ -42,75 +42,86 @@ import com.ade.util.OAuthUtil;
 import com.ade.restapi.UpdateInterface;
 import com.ade.restapi.SohuUpdate;
 
-import android.app.Activity;	
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;		
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;	
-import android.widget.EditText;	
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-
 public class MainActivity extends Activity implements SiteListener {
-	private final String TAG="MainActivity";
-	private final int AUTHREQUESTCODE=0;
-	private final int SITEERROR=0;
-	private final int TESTOK=1;
-	private User user=new User();
+	private final String TAG = "MainActivity";
+	private final int AUTHREQUESTCODE = 0;
+	private final int SITEERROR = 0;
+	private final int TESTOK = 1;
+	private User user = new User();
 	private Site site;
-	private int currentSite=SiteManager.SOHU;  //测试时注意修改此处为要测的网站
+	private int currentSite = SiteManager.SOHU; // 测试时注意修改此处为要测的网站
 	private View lastClickedView;
-	
-	private ResponseHandler<String> handler=new ResponseHandler<String>(){
+
+	private ResponseHandler<String> handler = new ResponseHandler<String>() {
 
 		@Override
 		public String handleResponse(HttpResponse response)
 				throws ClientProtocolException, IOException {
-			HttpResponse res=response;
+			HttpResponse res = response;
 			return null;
 		}
-		
+
 	};
-	
-	private Handler mainHandler=new Handler(new Handler.Callback() {
+
+	private Handler mainHandler = new Handler(new Handler.Callback() {
 		@Override
 		public boolean handleMessage(Message msg) {
-			switch(msg.what){
+			switch (msg.what) {
 			case SITEERROR:
-				String errorMessage=(String)msg.obj;
-				Toast.makeText(MainActivity.this, errorMessage!=null?errorMessage:"未知错误", Toast.LENGTH_LONG).show();
-				Intent intent=new Intent(MainActivity.this,OAuthActivity.class);
-				intent.putExtra("site",currentSite);
-				intent.putExtra("user",new User());
-				startActivityForResult(intent,AUTHREQUESTCODE);
+				String errorMessage = (String) msg.obj;
+				Toast.makeText(MainActivity.this,
+						errorMessage != null ? errorMessage : "未知错误",
+						Toast.LENGTH_LONG).show();
+				Intent intent = new Intent(MainActivity.this,
+						OAuthActivity.class);
+				intent.putExtra("site", currentSite);
+				intent.putExtra("user", new User());
+				startActivityForResult(intent, AUTHREQUESTCODE);
 				break;
 			case TESTOK:
-				Toast.makeText(MainActivity.this, "测试成功", Toast.LENGTH_LONG).show();
+				Toast.makeText(MainActivity.this, "测试成功", Toast.LENGTH_LONG)
+						.show();
 				Log.i(TAG, "测试成功");
 				break;
 			}
 			return false;
 		}
 	});
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {	
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);	
-        
-        SiteManager.getInstance().setContext(this);
-        SiteManager.getInstance().loadSites();
-        site=SiteManager.getInstance().getSites().get(currentSite);
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		SiteManager.getInstance().setContext(this);
+		SiteManager.getInstance().loadSites();
+		site = SiteManager.getInstance().getSites().get(currentSite);
 		site.addListener(MainActivity.this);
-        
-        findViewById(R.id.BtnWrite).setOnClickListener(new OnClickListener(){
+
+		findViewById(R.id.BtnWrite).setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+
+				Intent intentWrite = new Intent(MainActivity.this,
+						WriteActivity.class);
+				MainActivity.this.startActivity(intentWrite);
+				MainActivity.this.overridePendingTransition(
+						android.R.anim.slide_in_left,
+						android.R.anim.slide_out_right);
+			
 				lastClickedView=v;
 				//使用说明：通过如下代码可以进行发送微博的测试。首先会出现授权界面，授权后再点此按钮即可发送微博了。
 				//site.updateText("微博测试nancy,second");
@@ -119,9 +130,15 @@ public class MainActivity extends Activity implements SiteListener {
 				//然后使用如下代码.
 				//注意这里的TOKEN和SECRET对不同的用户名是不一样的，请仔细使用
 				user.setAccessToken("46b571ca341cfeb4737f419ed4ce0392");  
+
 				user.setAccessSecret("920143c011048ab9e4c8904440e7ed1a");
 				site.logIn(user);
 				site.friendsTimeline();
+
+				// site.updateText("上传微博");
+				// site.uploadImage("/data/data/com.ade.soda/10697.jpg","微博测试vv");
+				// 注意要先将欲上传的图片PUSH到模拟器对应的路径下
+
 				//site.updateText("上传微博");
 //				try {
 //					site.uploadImage("/data/data/com.ade.soda/10697.jpg","微博测试");
@@ -131,50 +148,110 @@ public class MainActivity extends Activity implements SiteListener {
 //					Log.i(TAG,"pic file not found." );
 //				}
 				//注意要先将欲上传的图片PUSH到模拟器对应的路径下
-			}
-        });
-        
-        findViewById(R.id.BtnRefresh).setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v) {
 
 			}
-        });
-     }
+		});
+		findViewById(R.id.TextViewMsg01).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Intent intentRE = new Intent(MainActivity.this,
+								REandFWActivity.class);
+						MainActivity.this.startActivity(intentRE);
+					}
+				});
+		findViewById(R.id.TextViewMsg02).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Intent intentRE = new Intent(MainActivity.this,
+								REandFWActivity.class);
+						MainActivity.this.startActivity(intentRE);
+					}
+				});
+		findViewById(R.id.TextViewMsg03).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Intent intentRE = new Intent(MainActivity.this,
+								REandFWActivity.class);
+						MainActivity.this.startActivity(intentRE);
+					}
+				});
+
+		findViewById(R.id.TextViewMsg04).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Intent intentRE = new Intent(MainActivity.this,
+								REandFWActivity.class);
+						MainActivity.this.startActivity(intentRE);
+					}
+				});
+		findViewById(R.id.BtnAllMsg).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						//todo
+					}
+				});
+		findViewById(R.id.BtnUser1Msg).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						//todo
+					}
+				});
+		findViewById(R.id.BtnUser2Msg).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						//todo
+					}
+				});
+		findViewById(R.id.BtnUserMore).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						//todo
+					}
+				});
+	}
 
 	@Override
 	public void onBeginRequest() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onError(String errorMessage) {
-		Message msg=new Message();
-		msg.what=SITEERROR;
-		msg.obj=errorMessage;
+		Message msg = new Message();
+		msg.what = SITEERROR;
+		msg.obj = errorMessage;
 		mainHandler.sendMessage(msg);
 	}
-
 
 	@Override
 	public void onResponsed() {
 		mainHandler.sendEmptyMessage(TESTOK);
-	}    
+	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode==AUTHREQUESTCODE){
-			if (resultCode==RESULT_OK){
-				if (data.hasExtra("user")){
-					user=(User)data.getSerializableExtra("user");
+		if (requestCode == AUTHREQUESTCODE) {
+			if (resultCode == RESULT_OK) {
+				if (data.hasExtra("user")) {
+					user = (User) data.getSerializableExtra("user");
 					site.logIn(user);
-					if (lastClickedView!=null){
+					if (lastClickedView != null) {
 						lastClickedView.performClick();
 					}
 				}
 			}
-			Toast.makeText(this, resultCode==RESULT_OK?"Auth Success":"Auth Fail", Toast.LENGTH_LONG).show();
+			Toast.makeText(this,
+					resultCode == RESULT_OK ? "Auth Success" : "Auth Fail",
+					Toast.LENGTH_LONG).show();
 		}
 		super.onActivityResult(requestCode, resultCode, data);
 	}
