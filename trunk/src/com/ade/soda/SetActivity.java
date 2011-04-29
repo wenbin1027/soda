68819f963fe43017adbbe39f3cb1d154e1df6a5b
david-zhang@david-zhang.net <david-zhang@david-zhang.net@1669ee13-3d97-0e45-1c2c-9cf43825c4fe>
2011-04-29 15:57:35 +0000

    Implement the user interaction on SetActivity.

--- trunk/src/com/ade/soda/SetActivity.java
+++ trunk/src/com/ade/soda/SetActivity.java
@@ -6,50 +6,52 @@ import com.ade.site.SiteManager;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;	
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
-
-import android.widget.Toast;
-
-public class SetActivity extends Activity implements OnItemClickListener{
-	private Site site;  //需要的话就用，不需要就删掉
+public class SetActivity extends Activity {
 	private ListView lView;
-	private	String providerNames[];
+	private	String providerNames[] = new String[] { getString(R.string.sina), getString(R.string.sohu)};
+	private int[] siteIds = new int[] {SiteManager.SINA, SiteManager.SOHU};
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Intent intent=getIntent();
-		if (intent!=null){
-			if (intent.hasExtra("site")){  //传进来的是当前site
-				site=SiteManager.getInstance().getSite(intent.getIntExtra("site", -1));
-			}
-		}
-
 		setContentView(R.layout.set);
 		lView = (ListView) findViewById(R.id.ListView01);
-		this.providerNames = new String[] { getString(R.string.sina), getString(R.string.sohu)};
 		lView.setAdapter(
-			new ArrayAdapter<String>(
-				this, 
-				android.R.layout.simple_list_item_multiple_choice, providerNames
-			)
+				new ArrayAdapter<String>(
+						this, 
+						android.R.layout.simple_list_item_multiple_choice, providerNames
+				)
 		);
 		lView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-		lView.setOnItemClickListener(this);
+		lView.setOnItemClickListener(this.mListCheckListener);
 	}
 
-	@Override
-	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-		// TODO Pass the account info to OAuthActivity
-		Toast.makeText(this, this.providerNames[arg2], Toast.LENGTH_LONG).show();
-	}
+	private OnItemClickListener mListCheckListener = new OnItemClickListener() {
+		public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+			SparseBooleanArray selectedIndexes = lView.getCheckedItemPositions();
+			Boolean itemChecked = selectedIndexes.get(arg2);
+			SiteManager siteMgr = SiteManager.getInstance();
+			Site currentSite = siteMgr.getSite(siteIds[arg2]);
 
+			if(itemChecked){
+				// Check the access key of site
+				String accessKey = currentSite.getAccessKey();
+				if(accessKey == null || accessKey == "")
+				{
+					// Pass the account info to OAuthActivity
+					Intent intent = new Intent(SetActivity.this, OAuthActivity.class);
+					intent.putExtra("site", siteIds[arg2]);
+					startActivity(intent);
+				}
+			}else{
+				// TODO Invalidate the access key
+			}
+		}			
+	};
 }
