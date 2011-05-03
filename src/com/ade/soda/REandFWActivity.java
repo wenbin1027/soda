d5ff90a233ea26c2f272ff7c640d890ef6062fea
nancy.yawei.li <nancy.yawei.li@gmail.com>
2011-05-03 05:41:31 +0000

    Update MainActivity.java and commit again with REandFWActivity.java

--- src/com/ade/soda/REandFWActivity.java
+++ src/com/ade/soda/REandFWActivity.java
@@ -10,8 +10,10 @@ import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.webkit.WebView;
 import android.widget.Button;	
 import android.widget.EditText;	
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -31,25 +33,59 @@ public class REandFWActivity extends Activity implements OnClickListener{
 				Log.i("REandFW", blog.toString());
 			}
 		}
-		else{//在开发时如果主界面还没开发完，可能传不过来Blog对象，可以使用这个else分支自己建一个Blog对象，但发布时不需要此else分支
-			//TODO:Delete this on RELEASE
-			blog=new Blog();
-			blog.setID(1234567);
-			blog.setText("仅供测试");
-			//......
+		
+		//display blog content 
+		WebView wvProfileImage=(WebView)findViewById(R.id.refwImage);
+		WebView wvBlogPic =(WebView) findViewById(R.id.refwPic);
+		WebView wvRetPic =(WebView) findViewById(R.id.refwRetPic);
+		TextView tvMsg = (TextView)findViewById(R.id.refwMsg); 
+		TextView tvUsrname = (TextView)findViewById(R.id.refwUsername);
+		TextView tvRetMsg = (TextView) findViewById(R.id.refwRetMsg);
+		
+		
+		tvMsg.setText(blog.getText());
+		
+		//sohu blog uses ScreenName instead of username
+		String blogName;
+		blogName=blog.getUser().getName();
+		if (blogName.equals("")){
+			blogName=blog.getUser().getScreenName();
 		}
-		findViewById(R.id.BtnRE).setOnClickListener(this);
-		findViewById(R.id.BtnFW).setOnClickListener(this);
-		findViewById(R.id.BtnMore).setOnClickListener(this);
-		findViewById(R.id.BtnFace).setOnClickListener(this);
-		findViewById(R.id.BtnImg).setOnClickListener(this);
-		findViewById(R.id.BtnOK).setOnClickListener(this);
+		
+		tvUsrname.setText( blogName );
+		wvProfileImage.loadUrl(blog.getUser().getProfileImageUrl());
+		
+		if (blog.getOriginalPic()!=null){
+			wvBlogPic.setVisibility(0);	
+			Log.i("REandFW",blog.getOriginalPic());
+			wvBlogPic.loadUrl(blog.getOriginalPic());
+		}
+		
+		//retweeted msg
+		Blog retBlog = blog.getRetweetedBlog();
+		if (retBlog != null){
+			tvRetMsg.setText(" \nRE:" + retBlog.getUser().getName() + "\n" + retBlog.getText());
+			if (retBlog.getOriginalPic() !=null){
+				wvRetPic.setVisibility(0);
+				Log.i("REandFW",retBlog.getOriginalPic());
+				wvRetPic.loadUrl(retBlog.getOriginalPic());
+			}
+		}
+		
+		
+		//findViewById(R.id.BtnRE).setOnClickListener(this);
+		//findViewById(R.id.BtnFW).setOnClickListener(this);
+		//findViewById(R.id.BtnMore).setOnClickListener(this);
+		//findViewById(R.id.BtnFace).setOnClickListener(this);
+		//findViewById(R.id.BtnImg).setOnClickListener(this);
+		//findViewById(R.id.BtnOK).setOnClickListener(this);
 
 		}
-	@Override
+	
+	//以下功能暂时不用
 	public void onClick(View v) {
 		
-		switch (v.getId()) {
+		/*switch (v.getId()) {
 		case R.id.BtnRE:
 			// todo
 			break;
@@ -71,6 +107,6 @@ public class REandFWActivity extends Activity implements OnClickListener{
 			break;
 		default:
 			break;
-		}
+		}*/
 	}
 }
