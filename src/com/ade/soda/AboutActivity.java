856ee48fa773375e8ee39c0ad18c29a309c9a722
bill.lv89 <bill.lv89@gmail.com>
2011-05-09 11:14:44 +0000

    初步完成退出提示，但字符串未能成功封装。关于界面的调用仍然存在问题，继续改进。

---
+++ src/com/ade/soda/AboutActivity.java
@@ -0,0 +1,13 @@
+package com.ade.soda;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity{
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+        }
+}
