966150fa957f3a2ca3e8fff816b9c97438baa53d
bill.lv89@gmail.com <bill.lv89@gmail.com@1669ee13-3d97-0e45-1c2c-9cf43825c4fe>
2011-08-02 16:44:40 +0000

    试编sohu评论的api部分，对于使用http协议传递参数的了解不甚细致，期待coding review指正

---
+++ trunk/src/com/ade/restapi/CommentInterface.java
@@ -0,0 +1,85 @@
+package com.ade.restapi;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.List;
+import org.apache.http.Header;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.StringEntity;
+import com.ade.util.OAuthUtil;
+import com.ade.parser.Parser;
+import com.ade.site.Site;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ * @created 10-����-2011 ���� 08:29:07
+ */
+public abstract class CommentInterface extends ApiInterface {
+
+	public CommentInterface(Parser parser) {
+		super(parser);
+	}
+
+
+	/**
+	 * 
+	 * @param text
+	 * @param site
+	 */
+	protected abstract Header[] getHeader(String text, Site site);
+
+
+	/**
+	 * 
+	 * @param text
+	 * @param site
+	 */
+	protected abstract String getUrl(String text, Site site);
+
+	protected abstract List<NameValuePair> getParams(String id,String text,Site site);
+	/**
+	 * 
+	 * @param text
+	 */
+	public boolean isValid(String text){
+		if (text==null)
+			return false;
+		int length=text.trim().length();
+		if (length<=0 || length>140)
+			return false;
+		return true;
+	}
+	
+	public HttpUriRequest getRequest(String text, Site site){
+		String textEncode=URLEncoder.encode(text);
+		String id=""+site.getSiteID();
+		String idEncode=URLEncoder.encode(id);
+		HttpPost request=new HttpPost(getUrl(textEncode,site));
+		Header[] headers=getHeader(textEncode,site);
+		if (headers!=null){
+			for(int i=0;i<headers.length;i++){
+				request.addHeader(headers[i]);
+			}
+		}
+		List<NameValuePair> data=getParams(idEncode,textEncode,site);
+		if (data!=null){
+			OAuthUtil.signRequest(getUrl(textEncode,site), request, data, 
+					site.getAppKey(), site.getAppSecret(), 
+					site.getAccessKey(), site.getAccessSecret());
+			StringEntity entity;
+			try {
+				entity = new UrlEncodedFormEntity(data);
+				request.setEntity(entity);
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return request;
+	}
+
+}
